15f73f625fe8ae2830d0a8c2e0e1a88519b863af
Vũ Phạm Tiến Dũng <43530592+vuphamtiendung@users.noreply.github.com>
2020-10-12 01:11:32 +0700

    Add files via upload
    Demo Crystal Report C# .Net

---
+++ CrystalReport_Demo_v0.1/CrystalReport_Demo_v0.1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace CrystalReport_Demo_v0._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            String strConnectionString = "Data Source = DESKTOP-5PSSEM5\\SQLEXPRESS; Initial Catalog=QLSINHVIEN4; Integrated Security=True";
+            RPDatasetSV rp = new RPDatasetSV();
+            rp.SetDataSource(dt);
+            crystalReportViewer1.ReportSource = rp;
+        }
+    }
+}
